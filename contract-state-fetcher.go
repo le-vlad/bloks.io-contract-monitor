4448eebb5ffaeb52d8d35b790de962237ea49e1b
Vladislav Len <heavenly.beta@gmail.com>
2021-04-22 09:32:37 +0300

    Update contract-state-fetcher.go

--- contract-state-fetcher.go
+++ contract-state-fetcher.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const requestURL = "https://eos.greymass.com/v1/chain/get_account"
+const requestURL = "https://eos-node.upland.me/v1/chain/get_account"
 
 type requestStateBody struct {
 	ContractName string `json:"account_name"`
